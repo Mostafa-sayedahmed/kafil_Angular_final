16b3532bdc02702515d691d98ba94d9d5ac9e0ba
Mostafa-sayedahmed <107958289+Mostafa-sayedahmed@users.noreply.github.com>
2023-03-30 22:57:48 +0200

    revert Merge branch 'backup' into mostafam

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,21 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ServicesComponent } from './components/services/services.component';
 import { SingleserviceComponent } from './components/singleservice/singleservice.component';
-import { LoginComponent } from './component/login/login.component';
-import { RegisterComponent } from './component/register/register.component';
-import { ForgetPasswordComponent } from './component/forget-password/forget-password.component';
-import { HomePageComponent } from './component/homePage/home-page/home-page.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: '', component: HomePageComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'services/:serviceID', component: SingleserviceComponent },
-  // { path: '', redirectTo: '/register-user', pathMatch: 'full' },
-  { path: 'sign-in', component: LoginComponent },
-  { path: 'register-user', component: RegisterComponent },
-  { path: 'forgot-password', component: ForgetPasswordComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -8,28 +8,14 @@ import { ServicesComponent } from './components/services/services.component';
 import { FormsModule } from '@angular/forms';
 import { CardsliderComponent } from './components/cardslider/cardslider.component';
 import { ServicecardComponent } from './components/servicecard/servicecard.component';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { SingleserviceComponent } from './components/singleservice/singleservice.component';
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    ServicesComponent,
-    CardsliderComponent,
-    ServicecardComponent,
-    SingleserviceComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
-  ],
+  declarations: [AppComponent, HeaderComponent, ServicesComponent, CardsliderComponent, ServicecardComponent, SingleserviceComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
   providers: [],
   bootstrap: [AppComponent],
 })
