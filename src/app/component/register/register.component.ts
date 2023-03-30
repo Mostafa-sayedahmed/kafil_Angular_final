0ba5fd01d48e81e16cc2616333ac5c813eacdee2
EsraaMokhtar <89299244+EsraaMokhtar@users.noreply.github.com>
2023-03-30 12:07:36 +0200

    register in firebase and login , forget password and login with google
    register in firebase and login with this new register , forget password and send email to your email to reset the password and login with google email

--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
-
+export class RegisterComponent implements OnInit {
+  constructor(
+    public UserService : UserService
+  ) { }
+  ngOnInit() { }
 }
