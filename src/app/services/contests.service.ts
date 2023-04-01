7188b66bbf4a39bb8f67dbdf4386f62f432a0848
EsraaMokhtar <89299244+EsraaMokhtar@users.noreply.github.com>
2023-04-01 12:06:03 +0200

    function get contests by section id

--- src/app/services/contests.service.ts
+++ src/app/services/contests.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { collection,Firestore } from '@angular/fire/firestore';
+import { collection,Firestore , query, where, getDocs,} from '@angular/fire/firestore';
+
 import { collectionData } from '@angular/fire/firestore';
 import { Icontest } from './../models/icontest';
+import { IcontestSection } from './../models/icontestsection';
 
 import { Observable } from 'rxjs';
 
@@ -11,11 +13,37 @@ import { Observable } from 'rxjs';
 export class ContestsService {
 
   contests : Icontest[] = []
-  constructor(private firestore: Firestore) { }
+  contestSections : IcontestSection[] = []
+  constructor(private firestore: Firestore ) { }
   
   getContests() {
     let contest = collection(this.firestore, "contests")
     return collectionData(contest, { idField: "id" }) as Observable<Icontest[]>
   }
 
+
+ async getContestsBySectionId(sectionId: string) {
+      const q = query(collection(this.firestore, "contests"), where('sectionId', '==', sectionId));
+
+      const querySnapshot = await getDocs(q);
+
+      var newArr : Array<object> = [];
+
+      querySnapshot.forEach((doc) => {
+        
+        newArr.push(doc.data());
+
+      });
+
+      return newArr;
+
+  }
+
+  getContestSections() {
+    let contestSections = collection(this.firestore, "contestSections")
+    return collectionData(contestSections, { idField: "id" }) as Observable<IcontestSection[]>
+  }
+  
+
+  
 }
