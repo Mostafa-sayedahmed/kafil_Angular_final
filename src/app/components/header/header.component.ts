f88b82e0a2d56b2631c623600efada8a45880298
Mostafa-sayedahmed <107958289+Mostafa-sayedahmed@users.noreply.github.com>
2023-04-06 13:52:18 +0200

    header login logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { GetservicesService } from 'src/app/services/getservices.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit, DoCheck {
+  user = {
+    apiKey: '',
+    appName: '',
+    createdAt: '',
+    displayName: '  ',
+    email: '',
+    emailVerified: '',
+    isAnonymous: '',
+    lastLoginAt: '',
+    photoURL: '',
+    providerData: [{}],
+    stsTokenManager: {
+      refreshToken: '',
+      accessToken: '',
+      expirationTime: '',
+    },
+    uid: '',
+  };
+  username = '';
+  photo = '';
+  constructor(private userservice: UserService) {}
+  ngDoCheck(): void {
+    this.user = JSON.parse(localStorage.getItem('user')!);
+    // console.log(this.user.displayName);
+  }
+  ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem('user')!);
+    // console.log(this.user.displayName);
+  }
+  logout() {
+    this.userservice.SignOut();
+  }
+}
