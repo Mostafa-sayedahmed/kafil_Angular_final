6633c8d6c28920071897e9daaacb1afa41350b8b
Ahmed A basset <107061768+Ahmedbm98@users.noreply.github.com>
2023-04-06 17:52:00 +0200

    create projects

---
+++ src/app/projectsDetails/projectsDetails.component.ts
@@ -0,0 +1,20 @@
+import { Component , OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-projectsDetails',
+  templateUrl: './projectsDetails.component.html',
+  styleUrls: ['./projectsDetails.component.scss']
+})
+export class ProjectsDetailsComponent {
+
+
+  constructor(){}
+
+        // ngOnInit(){
+
+        // })
+    }
+
+
+
